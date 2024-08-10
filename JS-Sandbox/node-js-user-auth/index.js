33d886f5a2ccfe6b38709dda69fc4f0b8983d701
Miguel Mayans <miguelmayansv@gmail.com>
2024-08-10 09:54:45 +0200

    First Commit

---
+++ JS-Sandbox/node-js-user-auth/index.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import { PORT } from './config.js'
+import { UserRespository } from './user-respository.js'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Working...')
+})
+
+app.get('/protected', (req, res) => {})
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body
+
+  try {
+    const user = await UserRespository.login({ username, password })
+    res.send({ user })
+  } catch (error) {
+    res.status(401).send(error.message)
+  }
+})
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body
+
+  try {
+    const id = await UserRespository.create({ username, password })
+    res.send({ id })
+  } catch (error) {
+    res.status(400).send(error.message)
+  }
+})
+
+app.post('/logout', (req, res) => {})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
