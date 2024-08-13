59d9067380ea9045500962aaa392ab15be31ed6a
Miguel Mayans <miguelmayansv@gmail.com>
2024-08-13 14:47:40 +0200

    added: Turso dB implementation

--- JS-Sandbox/realtime-chat-websockets/server/index.js
+++ JS-Sandbox/realtime-chat-websockets/server/index.js
@@ -1,15 +1,61 @@
-import express from "express";
-import logger from "morgan";
+import express from "express"
+import logger from "morgan"
+import dotenv from "dotenv"
+import { createClient } from "@libsql/client"
+import { Server } from "socket.io"
+import { createServer } from "http"
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
+dotenv.config()
 
-const app = express();
-app.use(logger("dev"));
+const app = express()
+const server = createServer(app)
+
+const io = new Server(server, {
+  connectionStateRecovery: { maxDisconnectionDuration: 6000 },
+})
+
+const db = createClient({
+  url: "libsql://selected-lava-man-miguelmayans.turso.io",
+  authToken: process.env.DB_TOKEN,
+})
+
+await db.execute(`
+    CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      content TEXT
+    )
+    `)
+
+io.on("connection", (socket) => {
+  console.log("User connected")
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected")
+  })
+
+  socket.on("chat message", async (msg) => {
+    let result
+    try {
+      result = await db.execute({
+        sql: "INSERT INTO messages (content) VALUES (:msg)",
+        args: { msg },
+      })
+    } catch (error) {
+      console.error(error)
+      return
+    }
+    io.emit("chat message", msg, result.lastInsertRowid.toString())
+  })
+})
+
+app.use(logger("dev"))
+app.use(express.static("client"))
 
 app.get("/", (req, res) => {
-  res.sendFile(process.cwd() + "/client/index.html");
-});
+  res.sendFile(process.cwd() + "/client/index.html")
+})
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
