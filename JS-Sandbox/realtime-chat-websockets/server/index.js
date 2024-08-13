2de1be2b9868a9366a01a5474089f15d46c16f52
Miguel Mayans <miguelmayansv@gmail.com>
2024-08-13 23:27:09 +0200

    added: username feature

--- JS-Sandbox/realtime-chat-websockets/server/index.js
+++ JS-Sandbox/realtime-chat-websockets/server/index.js
@@ -23,11 +23,12 @@ const db = createClient({
 await db.execute(`
     CREATE TABLE IF NOT EXISTS messages (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
-      content TEXT
+      content TEXT,
+      user TEXT
     )
     `)
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   console.log("User connected")
 
   socket.on("disconnect", () => {
@@ -36,17 +37,35 @@ io.on("connection", (socket) => {
 
   socket.on("chat message", async (msg) => {
     let result
+    let username = socket.handshake.auth.user ?? "Anonymous"
+
     try {
       result = await db.execute({
-        sql: "INSERT INTO messages (content) VALUES (:msg)",
-        args: { msg },
+        sql: "INSERT INTO messages (content, user) VALUES (:msg, :username)",
+        args: { msg, username },
       })
     } catch (error) {
       console.error(error)
       return
     }
-    io.emit("chat message", msg, result.lastInsertRowid.toString())
+    io.emit("chat message", msg, result.lastInsertRowid.toString(), username)
   })
+
+  if (!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: "SELECT id, content, user FROM messages where id > ?",
+        args: [socket.handshake.auth.serverOffset ?? 0],
+      })
+
+      results.rows.forEach((row) => {
+        socket.emit("chat message", row.content, row.id.toString(), row.user)
+      })
+    } catch (error) {
+      console.error(error)
+      return
+    }
+  }
 })
 
 app.use(logger("dev"))
