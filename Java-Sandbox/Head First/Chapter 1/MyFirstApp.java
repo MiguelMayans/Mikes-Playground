a1d0d812bc75ef5f0461f5847151fe31609b9d12
Miguel Mayans <miguelmayansv@gmail.com>
2025-04-16 11:17:36 +0200

    Update MyFirstApp

--- Java-Sandbox/Head First/Chapter 1/MyFirstApp.java
+++ Java-Sandbox/Head First/Chapter 1/MyFirstApp.java
@@ -2,5 +2,9 @@ public class MyFirstApp {
   public static void main(String[] args) {
     System.out.println("I Rule!");
     System.out.println("The World");
+    String name = "Mik";
+    if (name.equals("Mik")) {
+      System.out.println("Your name is Mik");
+    }
   }
 }
